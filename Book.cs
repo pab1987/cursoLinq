816cd8be0149f7e8c1e7fbac47df55493d7cdf13
Pablo Lara <pablo06lara87@gmail.com>
2023-08-24 09:14:45 -0500

    first commit

---
+++ Book.cs
@@ -0,0 +1,14 @@
+public class Book
+{
+    public string Tittle { get; set; }
+
+    public int PageCount { get; set; }
+
+    public string Status { get; set; }
+
+    public DateTime PublishedDate { get; set; }
+
+    public string[] Authors { get; set; }
+
+    public string[] Categories { get; set; }
+}
---
+++ LinqQueries.cs
@@ -0,0 +1,7 @@
+class LinqQueries
+{
+    public LinqQueries()
+    {
+            
+    }
+}
